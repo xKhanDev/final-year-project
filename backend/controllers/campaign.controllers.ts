369f58f4adc3c43f8a7d22d15876839e6851e226
xKhanDev <safeerkhanxofficial@gmail.com>
2024-12-16 12:38:48 +0500

    get all campaigns controller

--- backend/controllers/campaign.controllers.ts
+++ backend/controllers/campaign.controllers.ts
@@ -1,13 +1,23 @@
+import Campaign from "../models/campaign.model.ts";
+import ApiError from "../utils/ApiError.ts";
+import ApiResponse from "../utils/ApiResponse.ts";
 import AsyncHandler from "../utils/AsyncHandler.ts";
+import { Request,Response } from "express";
 
 // get all campaigns
-export const campaigns = AsyncHandler(async(req,res)=>{
-    
+export const campaigns = AsyncHandler(async(req:Request,res:Response):Promise<any>=>{
+    const campaigns = await Campaign.find({});
+    if(!campaigns) return new ApiError(404,"Campaigns not found");
+    return new ApiResponse(200,campaigns);
 })
 
 // upload campaign
-export const uploadCampaign = AsyncHandler(async(req,res)=>{
+export const uploadCampaign = AsyncHandler(async(req,res):Promise<any>=>{
+    const {title,category,description,goalAmount,achievedAmount} =req.body;
 
+    const emptyFeilds = [title,category,description,goalAmount,achievedAmount].some((feild) => feild?.trim(" ") === "");
+
+    if(!emptyFeilds) return new ApiError(400,"All fields are mandatory");
 })
 
 // edit campaign
